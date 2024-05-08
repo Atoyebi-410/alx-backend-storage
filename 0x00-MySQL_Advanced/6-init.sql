ad4f1903be8b8b50b67d201a28c3ee266326dc4e
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-05-08 16:21:32 +0000

    advanced sql

---
+++ 0x00-MySQL_Advanced/6-init.sql
@@ -0,0 +1,99 @@
+
+
+UPDATE users SET valid_email = 1 WHERE email = "bob@dylan.com";
+UPDATE users SET email = "sylvie+new@dylan.com" WHERE email = "sylvie@dylan.com";
+UPDATE users SET name = "Jannis" WHERE email = "jeanne@dylan.com";
+
+SELECT "--";
+SELECT * FROM users;
+
+UPDATE users SET email = "bob@dylan.com" WHERE email = "bob@dylan.com";
+
+SELECT "--";
+SELECT * FROM users;
+
+bob@dylan:~$ 
+bob@dylan:~$ cat 5-main.sql | mysql -uroot -p holberton 
+Enter password: 
+id  email   name    valid_email
+1   bob@dylan.com   Bob 0
+2   sylvie@dylan.com    Sylvie  1
+3   jeanne@dylan.com    Jeanne  1
+--
+--
+id  email   name    valid_email
+1   bob@dylan.com   Bob 1
+2   sylvie+new@dylan.com    Sylvie  0
+3   jeanne@dylan.com    Jannis  1
+--
+--
+id  email   name    valid_email
+1   bob@dylan.com   Bob 1
+2   sylvie+new@dylan.com    Sylvie  0
+3   jeanne@dylan.com    Jannis  1
+bob@dylan:~$ 
+Repo:
+
+GitHub repository: alx-backend-storage
+Directory: 0x00-MySQL_Advanced
+File: 5-valid_email.sql
+ Done?   Get a sandbox
+6. Add bonus
+mandatory
+Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+
+Requirements:
+
+Procedure AddBonus is taking 3 inputs (in this order):
+user_id, a users.id value (you can assume user_id is linked to an existing users)
+project_name, a new or already exists projects - if no projects.name found in the table, you should create it
+score, the score value for the correction
+Context: Write code in SQL is a nice level up!
+
+bob@dylan:~$ cat 6-init.sql
+-- Initial
+DROP TABLE IF EXISTS corrections;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+
+CREATE TABLE IF NOT EXISTS users (
+    id int not null AUTO_INCREMENT,
+    name varchar(255) not null,
+    average_score float default 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+    id int not null AUTO_INCREMENT,
+    name varchar(255) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS corrections (
+    user_id int not null,
+    project_id int not null,
+    score int default 0,
+    KEY `user_id` (`user_id`),
+    KEY `project_id` (`project_id`),
+    CONSTRAINT fk_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    CONSTRAINT fk_project_id FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE
+);
+
+INSERT INTO users (name) VALUES ("Bob");
+SET @user_bob = LAST_INSERT_ID();
+
+INSERT INTO users (name) VALUES ("Jeanne");
+SET @user_jeanne = LAST_INSERT_ID();
+
+INSERT INTO projects (name) VALUES ("C is fun");
+SET @project_c = LAST_INSERT_ID();
+
+INSERT INTO projects (name) VALUES ("Python is cool");
+SET @project_py = LAST_INSERT_ID();
+
+
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_c, 80);
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_py, 96);
+
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_c, 91);
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_py, 73);
